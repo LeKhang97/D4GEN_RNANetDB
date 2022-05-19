abd6b8a2174755cdb54405b5c612068f93332a54
FranGASTRIN <gastrin.francois@gmail.com>
2022-05-19 22:50:58 +0200

    Length and Resolution filter

--- filtering/pdb_filter.php
+++ filtering/pdb_filter.php
@@ -58,7 +58,7 @@ $pdb_csv = array_unique($pdbs_csv);
     $all_result = array_intersect($pdb_sql, $pdb_csv); // 'pdb_id' present in the 2 lists (SQLite and csv)
 //print_r($all_result);
     foreach ($all_result as $rows){
-        $my_table = $dbconn->query("SELECT * FROM structure WHERE pdb_id = $row");
+        $my_table = $dbconn->query("SELECT * FROM structure WHERE pdb_id == $row");
         echo $row['pdb_id']." | ".$row['pdb_model']." | ".$row['date']." | ".$row['exp_method']." | ".$row['resolution']
         echo "<br>"
         /*echo $rows;
