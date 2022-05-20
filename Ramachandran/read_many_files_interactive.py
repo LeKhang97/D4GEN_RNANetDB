ddd04bce9732d49d88c2de7f757f01b1f3b164fd
LeKhang97 <70560694+LeKhang97@users.noreply.github.com>
2022-05-20 23:22:32 +0200

    Update read_many_files_interactive.py

--- Ramachandran/read_many_files_interactive.py
+++ Ramachandran/read_many_files_interactive.py
@@ -11,6 +11,8 @@ from matplotlib.pyplot import figure
 import math 
 import random
 import chart_studio
+import plotly.offline
+import plotly.express as px
 #chart_studio.tools.set_credentials_file(username='Lequockhang', api_key='fH1SQ9xzJzvrqpU3KK7y')
 
 path = '/home/khangle/Downloads/RNANet'
@@ -28,18 +30,25 @@ def plot_multi_files(input):
     sub_name = []
     sub_eta = []
     sub_theta = []
+    sub_method = []
+    sub_res = []
     for p,v in enumerate(df[a]):
         if(ls_input[0] <= v and ls_input[1] >= v):
             sub_name += df['name'][p]
             sub_eta += df['eta'][p]
             sub_theta += df['theta'][p]
-        
-    return [sub_eta,sub_theta]
+            sub_method += [df['method'][p]]
+            sub_res += [df['resolution'][p]]
+    
+    return [sub_eta,sub_theta,sub_method, sub_res]
 
 len_input = sys.argv[1]
 
 result = plot_multi_files(len_input)
 
+os.chdir('/var/www/html/')
+
+#Ramachandran
 x = [180/math.pi*v - 180 for p,v in enumerate(result[0]) if(math.isnan(v) == False 
                                                     and math.isnan(result[1][p]) == False)]
 
@@ -54,15 +63,26 @@ while(sys.getsizeof(sub_y) < 524288/40):
     sub_x += [x[ran_pos[i]]]
     sub_y += [y[ran_pos[i]]]
     i+=1
-    
-#import chart_studio.plotly as py
-import plotly.figure_factory as ff
 
 colorscale = ['#7A4579', '#D56073', 'rgb(236,158,105)', (1, 1, 0.2), (0.98,0.98,0.98)]
-
 fig = ff.create_2d_density(
     sub_x, sub_y, title = 'Ramachandran Plot', colorscale=colorscale,
     hist_color='rgb(255, 237, 222)', point_size=3)
-import plotly.offline
-os.chdir('/var/www/html/')
+    
 plotly.offline.plot(fig, filename='output_plotly.html')
+
+#Pie chart
+Pie_method = list(set(result[2]))
+Pie_method_count = [result[2].count(i) for i in Pie_method]
+dict_data = {'Method': Pie_method, 'Count': Pie_method_count} 
+data = pd.DataFrame(dict_data)
+fig = px.pie(data, values='Count', names='Method', title='Experiment methods')
+plotly.offline.plot(fig, filename='output_plotly2.html')
+
+#Histogram
+dict_data2 = {'Method': result[2] , 'Res': result[3]} 
+data2 = pd.DataFrame(dict_data2)
+fig = px.histogram(data2, x="Res", color="Method")
+plotly.offline.plot(fig, filename='output_plotly3.html')
+
+
